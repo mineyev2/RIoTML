80a1974293977cad5a9ba1c3c96de4912bdb2e06
mineyev2 <mineyev2@illinois.edu>
2020-04-05 00:44:10 -0500

    added threads so processes can run at the same time

--- rpi_group_tracking.py
+++ rpi_group_tracking.py
@@ -7,6 +7,7 @@ import cv2
 import imutils
 import time
 import pantilthat
+import threading
 
 import socket, select, string, sys
 
@@ -16,8 +17,7 @@ def display() :
 	sys.stdout.write(you)
 	sys.stdout.flush()
 
-def main():
-
+def ball_tracking():
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-b", "--buffer", type=int, default=64,
@@ -39,102 +39,6 @@ def main():
     # allow the camera or video file to warm up
     time.sleep(2.0)
 
-    #server setup (client side)
-    if len(sys.argv) < 2:
-        host = input("Enter host ip address: ")
-        print(host)
-    else:
-        host = sys.argv[1]
-
-    port = 12000
-
-    # asks for user name
-    name = input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-
-    # connecting host
-    try:
-        s.connect((host, port))
-    except:
-        print("\33[31m\33[1m Can't connect to the server \33[0m")
-        sys.exit()
-
-    # if connected
-    s.send(name.encode('utf-8'))
-    display()
-    while 1:
-        socket_list = [sys.stdin, s]
-
-        # Get the list of sockets which are readable
-        rList, wList, error_list = select.select(socket_list, [], [])
-
-        for sock in rList:
-            # incoming message from server
-            if sock == s:
-                data = sock.recv(4096).decode()
-                if not data:
-                    print('\33[31m\33[1m \rDISCONNECTED!!\n \33[0m')
-                    sys.exit()
-                else:
-                    sys.stdout.write(data)
-                    display()
-
-            # user entered a message
-            else:
-                msg = sys.stdin.readline()
-                s.send(msg.encode('utf-8'))
-                display()
-
-        #server side while loop section
-        frame = vs.read()
-
-        # handle the frame from VideoCapture or VideoStream
-        frame = frame[1] if args.get("video", False) else frame
-
-        # resize the frame, blur it, and convert it to the HSV
-        # color space
-        frame = imutils.resize(frame, width=600)
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-        # construct a mask for the color "yellow", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = None
-
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-            pantilthat.pan(pantilthat.get_pan() + (center[0] - 300) / 50)
-            pantilthat.tilt(pantilthat.get_tilt() - (center[1] - 240) / 50)
-
-        # update the points queue
-        pts.appendleft(center)
-
-        key = cv2.waitKey(1) & 0xFF
-
-        # if the 'q' key is pressed, stop the loop
-        if key == ord("q"):
-            break
-
-'''
     # keep looping
     while True:
         # grab the current frame
@@ -184,7 +88,65 @@ def main():
         # if the 'q' key is pressed, stop the loop
         if key == ord("q"):
             break
-'''
+
+def main():
+
+    #server setup (client side)
+    if len(sys.argv) < 2:
+        host = input("Enter host ip address: ")
+        print(host)
+    else:
+        host = sys.argv[1]
+
+    port = 12000
+
+    # asks for user name
+    name = input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+
+    # connecting host
+    try:
+        s.connect((host, port))
+    except:
+        print("\33[31m\33[1m Can't connect to the server \33[0m")
+        sys.exit()
+
+    # if connected
+    s.send(name.encode('utf-8'))
+    display()
+
+    thread = threading.Thread(target=ball_tracking())
+    thread.start()
+
+
+    while 1:
+        socket_list = [sys.stdin, s]
+
+        # Get the list of sockets which are readable
+        rList, wList, error_list = select.select(socket_list, [], [])
+
+        for sock in rList:
+            # incoming message from server
+            if sock == s:
+                data = sock.recv(4096).decode()
+                if not data:
+                    print('\33[31m\33[1m \rDISCONNECTED!!\n \33[0m')
+                    sys.exit()
+                else:
+                    sys.stdout.write(data)
+                    display()
+
+            # user entered a message
+            else:
+                msg = sys.stdin.readline()
+                s.send(msg.encode('utf-8'))
+                display()
+
+
+    thread.join()
+    print("process completed")
+
 
 if __name__ == "__main__":
     main()
