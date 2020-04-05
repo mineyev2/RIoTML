752fca4a9104ed0f058bfef21ef79cd328e386f1
mineyev2 <mineyev2@illinois.edu>
2020-04-05 01:42:58 -0500

    j

--- rpi_group_tracking.py
+++ rpi_group_tracking.py
@@ -144,8 +144,8 @@ def main():
     jobs = []
 
     #starting up ball tracking thread
-    ball_thread = multiprocessing.Thread(target=ball_tracking())
-    client_thread = multiprocessing.Thread(target=client, args=(name, host,))
+    ball_thread = multiprocessing.Process(target=ball_tracking())
+    client_thread = multiprocessing.Process(target=client, args=(name, host, ))
     jobs.append(client_thread)
     jobs.append(ball_thread)
 
