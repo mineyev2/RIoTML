8e3f2298686719f8f434c3c686adfdf277eabe3d
mineyev2 <mineyev2@illinois.edu>
2020-04-05 00:18:05 -0500

    changed rpi group tracking

--- rpi_group_tracking.py
+++ rpi_group_tracking.py
@@ -7,88 +7,92 @@ import cv2
 import imutils
 import time
 import pantilthat
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--buffer", type=int, default=64,
-    help="max buffer size")
-args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (0, 150, 150)
-greenUpper = (64, 255, 255)
-pts = deque(maxlen=args["buffer"])
-
-vs = VideoStream(src=0).start()
- 
-pantilthat.pan(0)
-pantilthat.tilt(0)
- 
-# allow the camera or video file to warm up
-time.sleep(2.0)
+import socket, select, string, sys
 
 
+def main():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-b", "--buffer", type=int, default=64,
+                    help="max buffer size")
+    args = vars(ap.parse_args())
 
-# keep looping
-while True:
-    # grab the current frame
-    frame = vs.read()
- 
-    # handle the frame from VideoCapture or VideoStream
-    frame = frame[1] if args.get("video", False) else frame
- 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
- 
-    # construct a mask for the color "yellow", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
+    # define the lower and upper boundaries of the "green"
+    # ball in the HSV color space, then initialize the
+    # list of tracked points
+    greenLower = (0, 150, 150)
+    greenUpper = (64, 255, 255)
+    pts = deque(maxlen=args["buffer"])
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            
-        pantilthat.pan(pantilthat.get_pan()+(center[0]-300)/50)
-        pantilthat.tilt(pantilthat.get_tilt()-(center[1]-240)/50)
+    vs = VideoStream(src=0).start()
 
-    # update the points queue
-    pts.appendleft(center)
+    pantilthat.pan(0)
+    pantilthat.tilt(0)
 
-    key = cv2.waitKey(1) & 0xFF
- 
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
- 
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-    vs.stop()
- 
-# otherwise, release the camera
-else:
-    vs.release()
- 
-# close all windows
-cv2.destroyAllWindows()
+    # allow the camera or video file to warm up
+    time.sleep(2.0)
 
+    # keep looping
+    while True:
+        # grab the current frame
+        frame = vs.read()
+
+        # handle the frame from VideoCapture or VideoStream
+        frame = frame[1] if args.get("video", False) else frame
+
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color "yellow", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            pantilthat.pan(pantilthat.get_pan() + (center[0] - 300) / 50)
+            pantilthat.tilt(pantilthat.get_tilt() - (center[1] - 240) / 50)
+
+        # update the points queue
+        pts.appendleft(center)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
+
+    # if we are not using a video file, stop the camera video stream
+    if not args.get("video", False):
+        vs.stop()
+
+    # otherwise, release the camera
+    else:
+        vs.release()
+
+    # close all windows
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
