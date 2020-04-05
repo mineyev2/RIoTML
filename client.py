d74846dc30195f736e286f3a2d4571fd927a337c
mineyev2 <mineyev2@illinois.edu>
2020-04-05 00:26:57 -0500

    combined client and ball tracking i think...

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ def main():
     else:
         host = sys.argv[1]
 
-    port = 11000
+    port = 12000
     
     #asks for user name
     name=input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
