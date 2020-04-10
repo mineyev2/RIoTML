126ba83a6d8448ecfa0dbf1d7ad6317a9d1f42ca
mineyev2 <mineyev2@illinois.edu>
2020-04-10 18:47:10 -0500

    its working, now adding customizations so that it is more suitable for us

--- no_threading_tracking.py
+++ no_threading_tracking.py
@@ -124,7 +124,7 @@ def main():
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             if(not found):
-                msg="found the ball")
+                msg="found the ball"
                  s.send(msg.encode('utf-8'))
                  display()
             # find the largest contour in the mask, then use
