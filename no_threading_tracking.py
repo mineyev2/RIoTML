af75bf814371a580bb9a894b841cac861950c3ed
mineyev2 <mineyev2@illinois.edu>
2020-04-10 19:50:21 -0500

    its working, now adding customizations so that it is more suitable for us

--- no_threading_tracking.py
+++ no_threading_tracking.py
@@ -124,7 +124,7 @@ def main():
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             if(not found):
-                msg='hell how are youo'
+                msg='hell how are youo \n'
                 print(msg.encode('utf-8'))
                 s.send(msg.encode('utf-8'))
                 display()
